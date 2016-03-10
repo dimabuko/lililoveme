0488c4bcce6b9acff041083e96d3306e2f2b9e97
dimabuko <bukodmitry@gmail.com>
2016-03-10 13:55:11 +0100

    desktop footer added

---
+++ footer.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<div class="footer_links footer_links_col1"><?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?></div>
+
+		<div class="footer_links footer_links_col2"><?php wp_nav_menu( array( 'theme_location' => 'footery' ) ); ?></div>
+		
+		<div class="footer_navigation_thirdcolumn">© 2016 — Lililove.me<br /><br />
+		Сервис разработан в Республике Беларусь ИП Подоляк Н.В. Свидетельство о регистрации Мингорисполкома от 13 октября 2014 в Едином государственном регистре юридических лиц и индивидуальных предпринимателей #192358340<br />
+<a href="contacts">Реквизиты и контактная информация</a>
+		</div>
+
+		<div class="footer_navigation_paymentlogos"><br /><img src="http://lililove.me/wp-content/uploads/2016/03/payment_logos.png" /><br /><br />Платежи осуществляются c высокой степенью защиты<br />c использованием протокола SSL 3.0<br />
+<a href="payment">Правила оплаты и возрата</a></div>
+
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+				<nav class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Primary Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'primary',
+							'menu_class'     => 'primary-menu',
+						 ) );
+					?>
+				</nav><!-- .main-navigation -->
+			<?php endif; ?>
+
+			<?php if ( has_nav_menu( 'social' ) ) : ?>
+				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentysixteen' ); ?>">
+					<?php
+						wp_nav_menu( array(
+							'theme_location' => 'social',
+							'menu_class'     => 'social-links-menu',
+							'depth'          => 1,
+							'link_before'    => '<span class="screen-reader-text">',
+							'link_after'     => '</span>',
+						) );
+					?>
+				</nav><!-- .social-navigation -->
+			<?php endif; ?>
+
+			<div class="site-info">
+				<?php
+					/**
+					 * Fires before the twentysixteen footer text for footer customization.
+					 *
+					 * @since Twenty Sixteen 1.0
+					 */
+					do_action( 'twentysixteen_credits' );
+				?>
+			</div><!-- .site-info -->
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- functions.php
+++ functions.php
@@ -4,4 +4,9 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+register_nav_menus( array(  
+  'primary' => __( 'Primary Navigation', 'twentysixteen' ),  
+  'footery' => __( 'Footery Navigation', 'twentysixteen' ), 
+  'secondary' => __('Secondary Navigation', 'twentysixteen')  
+) );
 ?>
